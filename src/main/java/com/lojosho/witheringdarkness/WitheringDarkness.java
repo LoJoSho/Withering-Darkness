6e352158898c409d911f74bd381d316f3eb9a8ae
LoJoSho <37521985+LoJoSho@users.noreply.github.com>
2020-11-28 22:49:52 -0600

    Add files via upload

--- src/main/java/com/lojosho/witheringdarkness/WitheringDarkness.java
+++ src/main/java/com/lojosho/witheringdarkness/WitheringDarkness.java
@@ -12,6 +12,7 @@ public final class WitheringDarkness extends JavaPlugin implements Listener {
     public void onEnable() {
         // Plugin startup logic
         getServer().getPluginManager().registerEvents(this, this);
+        this.saveDefaultConfig();
     }
 
     @EventHandler
@@ -19,8 +20,10 @@ public final class WitheringDarkness extends JavaPlugin implements Listener {
         if (!e.isCancelled()) {
             if (e.getPlayer().getGameMode() == GameMode.SURVIVAL) {
                 Location CheckBlock = e.getPlayer().getLocation();
-                if (CheckBlock.getBlock().getLightLevel() <= 5) {
-                    e.getPlayer().damage(1);
+                int lightRequired = this.getConfig().getInt("LightLevelRequired");
+                if (CheckBlock.getBlock().getLightLevel() <= lightRequired) {
+                    int damageGiven = this.getConfig().getInt("DamageGiven");
+                    e.getPlayer().damage(damageGiven);
                 }
             }
         }
