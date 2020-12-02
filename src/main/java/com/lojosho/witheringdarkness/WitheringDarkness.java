00c1a703d9569563eb4f24a2dd1581673867b43c
LoJoSho <37521985+LoJoSho@users.noreply.github.com>
2020-12-02 15:11:42 -0600

    Add files via upload

--- src/main/java/com/lojosho/witheringdarkness/WitheringDarkness.java
+++ src/main/java/com/lojosho/witheringdarkness/WitheringDarkness.java
@@ -1,33 +1,77 @@
 package com.lojosho.witheringdarkness;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
-import org.bukkit.WorldType;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
-import java.util.Objects;
+import java.util.List;
 
 public final class WitheringDarkness extends JavaPlugin {
+
     @Override
     public void onEnable() {
         // Plugin startup logic
-        //getServer().getPluginManager().registerEvents(this, this);
         this.saveDefaultConfig();
         int tickpercheck = this.getConfig().getInt("TicksPerCheck");
+        int tickspermessage = this.getConfig().getInt("TicksPerMessage");
         BukkitScheduler scheduler = getServer().getScheduler();
         scheduler.scheduleSyncRepeatingTask(this, this::RunCheck, 0L, tickpercheck);
+        scheduler.scheduleSyncRepeatingTask(this, this::runMessage, 0L, tickspermessage);
+        if (Bukkit.getVersion().contains("1.13")) {
+            getLogger().info("1.13 is an experimental version. It has a high chance to work, however, isn't 100%.");
+        }
+        if (Bukkit.getVersion().contains("1.14")) {
+            getLogger().info("1.14 is an experimental version. It has a high chance to work, however, isn't 100%.");
+        }
+        if (Bukkit.getVersion().contains("1.15")) {
+            getLogger().info("1.15 is an experimental version. It has a high chance to work, however, isn't 100%.");
+        }
     }
+
+    int damageGiven = this.getConfig().getInt("DamageGiven");
+    int BlockLightRequired = this.getConfig().getInt("BlockLightRequired");
+    int SkyLightRequired = this.getConfig().getInt("SkyLightRequired");
+    List<String> DisabledWorlds = this.getConfig().getStringList("Disabled-worlds");
+    String damageMessage = this.getConfig().getString("Damage-Message");
 
     public void RunCheck() {
         for (Player player : Bukkit.getOnlinePlayers()) {
-            int damageGiven = this.getConfig().getInt("DamageGiven");
-            int lightRequired = this.getConfig().getInt("LightLevelRequired");
             Location checkBlock = player.getLocation();
-            if (player.getGameMode() == GameMode.SURVIVAL && checkBlock.getBlock().getLightLevel() <= lightRequired && player.getLocation().getWorld().getWorldType() == WorldType.NORMAL) {
-                Objects.requireNonNull(player.getPlayer()).damage(damageGiven);
+            if (player.getGameMode() == GameMode.SURVIVAL) {
+                {
+                    if (!DisabledWorlds.contains(player.getWorld().getName()) && !player.hasPermission("wd.immune")) {
+                        if (checkBlock.getBlock().getLightLevel() <= BlockLightRequired) {
+                            player.damage(damageGiven);
+                        } else {
+                            if (checkBlock.getBlock().getLightLevel() <= SkyLightRequired) {
+                                player.damage(damageGiven);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void runMessage() {
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            Location checkBlock = player.getLocation();
+            if (player.getGameMode() == GameMode.SURVIVAL) {
+                {
+                    if (!DisabledWorlds.contains(player.getWorld().getName()) && !player.hasPermission("wd.immune")) {
+                        if (checkBlock.getBlock().getLightLevel() <= BlockLightRequired) {
+                            player.sendMessage(ChatColor.translateAlternateColorCodes('&', damageMessage));
+                        } else {
+                            if (checkBlock.getBlock().getLightLevel() <= SkyLightRequired) {
+                                player.sendMessage(ChatColor.translateAlternateColorCodes('&', damageMessage));
+                            }
+                        }
+                    }
+                }
             }
         }
     }
